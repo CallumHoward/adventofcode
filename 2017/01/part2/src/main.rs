1f825ecc91503dac725ba43645d8767360c7675b
CallumHoward <callumh@cse.unsw.edu.au>
2017-12-03 16:02:38 +1100

    solved part 2

---
+++ 2017/01/part2/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).expect("read error");
+
+    const RADIX: u32 = 10;
+    let v: Vec<u32> = input.chars()
+        .filter_map(|c| c.to_digit(RADIX)).collect();
+
+    println!("{}", sum_if_repeated(v));
+}
+
+fn sum_if_repeated(v: Vec<u32>) -> u32 {
+    let mut sum = 0;
+    let half = v.len() / 2;
+
+    for i in 0..v.len() {
+        if v[i] == v[(i + half) % v.len()] {
+            sum += v[i];
+        }
+    }
+    
+    sum
+}
