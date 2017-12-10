4c389c39ad89f904dcaa421b0608cdd41692c50b
CallumHoward <callumh@cse.unsw.edu.au>
2017-12-10 21:48:49 +1100

    part 1 formatting

--- 2017/02/part1/src/main.rs
+++ 2017/02/part1/src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let in_stream = stdin();
 
     for line in in_stream.lock().lines() {
-        let ns: Vec<u32> = line
-            .unwrap()
+        let ns: Vec<u32> = line.unwrap()
             .split_whitespace()
             .map(|s| s.parse().unwrap())
             .collect();
         sum += ns.iter().max().unwrap() - ns.iter().min().unwrap();
     }
+
     println!("{}", sum);
 }
