604d188f1e9e29f66caa2da8669c6e8d6d3efe53
CallumHoward <callumh@cse.unsw.edu.au>
2018-01-02 12:22:17 +1100

    part1 python impl

---
+++ 2017/07/part1.py
@@ -0,0 +1,19 @@
+# 07/part1.py
+# http://adventofcode.com/2017/day/7
+# Callum Howard, 2017
+
+import sys
+
+weights = dict()
+parent = dict()
+
+for line in sys.stdin.readlines():
+    name, weight, *children = ''.join(c for c in line if c.isalnum() or c == ' ').split()
+    weights[name] = int(weight)
+    for child in children:
+        parent[child] = name
+
+while name in parent:
+    name = parent[name]
+
+print(name)
